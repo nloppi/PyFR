a926538c32e53fba27ddcdbb0e07dea398860947
Niki Andreas Loppi <Niki@Niki-MPB.local>
2016-07-26 17:13:22 -0700

    Fixed rk4.

--- pyfr/integrators/dual/pseudosteppers.py
+++ pyfr/integrators/dual/pseudosteppers.py
@@ -26,7 +26,6 @@ class BaseDualPseudoStepper(BaseDualIntegrator):
         self._prepare_reg_banks(regs[0], currsoln, *self._source_regidx)
         self._queue % axnpby2(1, *svals)
 
-
     def finalise_step(self, currsoln):
         add = self._add
         pnreg = self._pseudo_stepper_nregs
@@ -121,20 +120,20 @@ class DualPseudoRK4Stepper(BaseDualPseudoStepper):
 
     @property
     def _pseudo_stepper_nregs(self):
-        return 4
+        return 3
 
     @property
     def _pseudo_stepper_order(self):
         return 4
 
     def step(self, t, dt, dtau):
-        add, add_with_dts = self._add, self._add_with_dts
+        add, add_with_dts  = self._add, self._add_with_dts
         rhs = self.system.rhs
         rat = dtau / dt
 
         # Get the bank indices for pseudo-registers (n+1,m; n+1,m+1; rhs),
         # where m = pseudo-time and n = real-time
-        r0, r1, r2, r3 = self._stepper_regidx
+        r0, r1, r2 = self._stepper_regidx
 
         # Ensure r0 references the bank containing u(n+1,m)
         if r0 != self._idxcurr:
@@ -143,32 +142,32 @@ class DualPseudoRK4Stepper(BaseDualPseudoStepper):
         # First stage; r1 = -∇·f(r0)
         rhs(t, r0, r1)
 
-        # Second stage; r2 = r0 + dtau/2*r1 - dtau/2*dQ/dt; r3 = -∇·f(r2)
+        # Second stage; r2 = r0 + dtau/2*r1 - dtau/2*dQ/dt; r2 = -∇·f(r2)
         add_with_dts(0, r2, 1, r0, dtau/2, r1, c=rat/2, currsoln=r0)
-        rhs(t, r2, r3)
+        rhs(t, r2, r2)
 
         # As no subsequent stages depend on the first stage we can
         # reuse its register to start accumulating the solution with
-        # r1 = r0 + dtau/6*r1 + dtau/3*r3
-        add(dtau/6, r1, 1, r0, dtau/3, r3)
+        # r1 = r0 + dtau/6*r1 + dtau/3*r2
+        add(dtau/6, r1, 1, r0, dtau/3, r2)
 
         # Third stage; here we reuse the r2 register
-        # r3 = r0 + dtau/2*r3 - dtau/2*dQ/dt
-        # r2 = -∇·f(r3)
-        add_with_dts(dtau/2, r3, 1, r0, c=rat/2, currsoln=r0)
-        rhs(t, r3, r2)
+        # r2 = r0 + dtau/2*r2 - dtau/2*dQ/dt
+        # r2 = -∇·f(r2)
+        add_with_dts(dtau/2, r2, 1, r0, c=rat/2, currsoln=r0)
+        rhs(t, r2, r2)
 
-        # Accumulate; r1 = r1 + dtau/3*r3
+        # Accumulate; r1 = r1 + dtau/3*r2
         add(1, r1, dtau/3, r2)
 
         # Fourth stage; again we reuse r2
-        # r3 = r0 + dtau*r3 - dtau*dQ/dt
-        # r2 = -∇·f(r3)
+        # r2 = r0 + dtau*r2 - dtau*dQ/dt
+        # r2 = -∇·f(r2)
         add_with_dts(dtau, r2, 1, r0, c=rat, currsoln=r0)
-        rhs(t, r2, r3)
+        rhs(t, r2, r2)
 
         # Final accumulation r1 = r1 + dtau/6*r2 - dtau*dQ/dt = u(n+1,m+1)
-        add_with_dts(1, r1, dtau/6, r3, c=rat, currsoln=r0)
+        add_with_dts(1, r1, dtau/6, r2, c=rat, currsoln=r0)
 
         # Return the index of the bank containing u(n+1,m+1)
         return r1
