b9780f9991e38efbbc27ef05aa061c8baef73edf
Niki Andreas Loppi <Niki@Niki-MPB.local>
2016-07-27 10:33:30 -0700

    Tiny bug in acns boundaryconditions

--- pyfr/solvers/acnavstokes/inters.py
+++ pyfr/solvers/acnavstokes/inters.py
@@ -77,12 +77,12 @@ class ACNavierStokesBaseBCInters(BaseAdvectionDiffusionBCInters):
 
         self.kernels['con_u'] = lambda: self._be.kernel(
             'bcconu', tplargs=tplargs, dims=[self.ninterfpts],
-            ulin=self._scal0_lhs, ulout=self._vect0_lhs,
+            ulin=self._scal_lhs, ulout=self._vect_lhs,
             nlin=self._norm_pnorm_lhs, ploc=self._ploc
         )
         self.kernels['comm_flux'] = lambda: self._be.kernel(
             'bccflux', tplargs=tplargs, dims=[self.ninterfpts],
-            ul=self._scal0_lhs, gradul=self._vect0_lhs,
+            ul=self._scal_lhs, gradul=self._vect_lhs,
             magnl=self._mag_pnorm_lhs, nl=self._norm_pnorm_lhs,
             ploc=self._ploc
         )
